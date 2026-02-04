f7495e3847744f2023b8989a9af19327fd91bc36
Tamara <tamaraforgive2@gmail.com>
2026-02-04 18:08:37 +0100

    add infographiste

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="flex flex-col justify-center md:gap-2 gap-3 text-center items-center">
             <p className="text-white text-sm max-sm:text-center">© Copyright 2025 - TAMARA EMOMI Seth Forgive. Tous droits réservés.</p>
             <div className="flex gap-1.5  text-center items-center justify-center">
-            <p className="text-white text-sm border-r-2 border-white pr-1.5">Plan du site</p>
+            <p className="text-white text-sm border-r-2 border-white pr-1.5">Plan du site designer par moi-même</p>
             <p className="text-white text-sm border-r-2 border-white pr-1.5"> Mentions légales</p>
             <p className="text-white text-sm"> Politique de confidentialité</p>
             </div>
